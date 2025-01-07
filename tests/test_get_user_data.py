74b5c3b598530c904254f8bce6780527fe35d704
AlekseiNovikov <62709853+alpan19811@users.noreply.github.com>
2025-01-07 21:14:18 +0300

    dd llure reporting to tests

--- tests/test_get_user_data.py
+++ tests/test_get_user_data.py
@@ -1,6 +1,7 @@
 import httpx
 from jsonschema import validate
 from core.contracts import USER_DATA_SCHEME
+import allure
 
 BASE_URL = "https://reqres.in/"
 LIST_USERS = "api/users?page=2"
@@ -9,25 +10,43 @@ SINGLE_USER_NOT_FOUND = "api/users/23"
 EMAIL_ENDS = "@reqres.in"
 AVATAR_ENDS = "-image.jpg"
 
+@allure.suite('Проверка запросов данных пользователей')
+@allure.title('Проверяем получение списка пользователей')
 def test_list_users():
-    response = httpx.get(BASE_URL + LIST_USERS)
-    assert response.status_code == 200
-    data = response.json()['data']
+    with allure.step(f'Делаем запрос по адресу: {BASE_URL + LIST_USERS}'):
+        response = httpx.get(BASE_URL + LIST_USERS)
 
+    with allure.step('Проверяем код ответа'):
+        assert response.status_code == 200
+
+    data = response.json()['data']
     for item in data:
-        validate(item, USER_DATA_SCHEME)
-        assert item['email'].endswith(EMAIL_ENDS)
-        assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS)
+        with allure.step(f'Проверяем элемент из списка'):
+            validate(item, USER_DATA_SCHEME)
+            with allure.step('Проверяем окончание Email адреса'):
+                assert item['email'].endswith(EMAIL_ENDS)
+            with allure.step('Проверяем наличие id в ссылке на аватарку'):
+                assert item['avatar'].endswith(str(item['id']) + AVATAR_ENDS)
 
+@allure.suite('Проверка запросов данных пользователей')
+@allure.title('Проверяем получение данных одного пользователя')
 def test_single_user():
-    response = httpx.get(BASE_URL + SINGLE_USER)
-    assert response.status_code == 200
-    data = response.json()['data']
+    with allure.step(f'Делаем запрос по адресу: {BASE_URL + SINGLE_USER}'):
+        response = httpx.get(BASE_URL + SINGLE_USER)
+    with allure.step('Проверка кода ответа'):
+        assert response.status_code == 200
+    with allure.step('Извлекаем данные пользователя'):
+        data = response.json()['data']
+    with allure.step('Извлекаем окончание Email адреса'):
+        assert data['email'].endswith(EMAIL_ENDS)
+    with allure.step('Проверяем наличие id в ссылке на аватарку'):
+        assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)
 
-    assert data['email'].endswith(EMAIL_ENDS)
-    assert data['avatar'].endswith(str(data['id']) + AVATAR_ENDS)
-
+@allure.suite('Проверка запросов данных пользователей')
+@allure.title('Проверяем пользователя, который не найден')
 def test_user_not_found():
-    response = httpx.get(BASE_URL + SINGLE_USER_NOT_FOUND)
-    assert response.status_code == 404
+    with allure.step(f'Делаем запрос по адресу: {BASE_URL + SINGLE_USER_NOT_FOUND}'):
+        response = httpx.get(BASE_URL + SINGLE_USER_NOT_FOUND)
+    with allure.step('Проверка кода ответа'):
+        assert response.status_code == 404
 
