09ceb29bddba6dc2225126e17260543407a2bc1e
AlekseiNovikov <62709853+alpan19811@users.noreply.github.com>
2025-01-13 22:24:12 +0300

    добавлена папка core

---
+++ core/contracts.py
@@ -0,0 +1,43 @@
+USER_DATA_SCHEME = {
+    "type" : "object",
+    "properties" : {
+        "id" : {"type": "number"},
+        "email" : {"type": "string"},
+        "first_name" : {"type": "string"},
+        "last_name" : {"type": "string"},
+        "avatar" : {"type": "string"}
+    },
+    "required" : ["id", "email", "first_name", "last_name", "avatar"]
+}
+
+RESOURCE_SCHEME = {
+    "type" : "object",
+    "properties" : {
+        "id" : {"type": "number"},
+        "name" : {"type": "string"},
+        "year" : {"type": "number"},
+        "color" : {"type": "string"},
+        "pantone_value" : {"type": "string"}
+    },
+    "required" : ["id", "name", "year", "color", "pantone_value"]
+}
+
+CREATED_USER_SCHEME = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+        "id": {"type": "string"}
+    },
+    "required": ["id"]
+}
+
+UPDATED_USER_SCHEME = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+        "updatedA": {"type": "string"}
+    },
+    "required": ["name", "job", "updatedAt"]
+}
