938cb17bc2d09b423da522b5e99db13365b76015
AlekseiNovikov <62709853+alpan19811@users.noreply.github.com>
2025-01-15 22:40:07 +0300

    Added test scripts for successful login, register, and unsuccessful register

---
+++ tests/test_register_and_login.py
@@ -0,0 +1,21 @@
+import json
+
+import httpx
+import pytest
+from jsonschema import validate
+from core.contracts import REGISTERED_USER_SCHEME
+
+BASE_URL = "https://reqres.in/"
+REGISTER_USERS = "api/register"
+
+json_file = open('C:/Users/User/PycharmProjects/ApiTests/core/new_users_data.json') #обращаемся к json-файлу
+users_data = json.load(json_file) #загружаем json-файл
+
+@pytest.mark.parametrize('users_data', users_data) #параметризация по "users_data"
+def test_successful_register(users_data):
+    response = httpx.post(BASE_URL + REGISTER_USERS, json=users_data)
+    assert response.status_code == 200
+
+    validate(response.json(), REGISTERED_USER_SCHEME)
+
+
