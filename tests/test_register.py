e5844f7c5400efb9a25c1a01649958f797f84d85
AlekseiNovikov <62709853+alpan19811@users.noreply.github.com>
2025-01-18 22:01:38 +0300

    обавил файл test_register.py

--- tests/test_register.py
+++ tests/test_register.py
@@ -5,6 +5,7 @@ import pytest
 from jsonschema import validate
 from core.contracts import REGISTERED_USER_SCHEME
 
+
 BASE_URL = "https://reqres.in/"
 REGISTER_USERS = "api/register"
 
@@ -13,10 +14,8 @@ users_data = json.load(json_file) #загружаем json-файл
 
 @pytest.mark.parametrize('users_data', users_data) #параметризация по "users_data"
 def test_successful_register(users_data):
-    headers = {'Content-Type': 'aplication/json'}
-    response = httpx.post(BASE_URL + REGISTER_USERS, json=users_data, headers=headers)
+    response = httpx.post(BASE_URL + REGISTER_USERS, json=users_data)
     assert response.status_code == 200
 
     validate(response.json(), REGISTERED_USER_SCHEME)
-
 
