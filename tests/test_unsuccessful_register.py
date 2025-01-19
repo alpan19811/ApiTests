6f878b511c7e6c83a6da86c801dd7cf3364d9fa1
AlekseiNovikov <62709853+alpan19811@users.noreply.github.com>
2025-01-19 20:56:59 +0300

    добавлен тест неуспешной регистрации пользователя

--- tests/test_unsuccessful_register.py
+++ tests/test_unsuccessful_register.py
@@ -1,20 +1,22 @@
 import httpx
+import allure
 from jsonschema import validate
 from core.contracts import UNSUCCESSFUL_REGISTER_SCHEME
 
 BASE_URL = "https://reqres.in/"
 REGISTER_USERS = "api/register"
-
+@allure.suite('Регистрация пользователей')
+@allure.title('Проверка ошибки при регистрации без пароля')
 def test_unsuccessful_register():
     request_data = {
         "email": "sydney@fife"
     }
-
-    response = httpx.post(BASE_URL + REGISTER_USERS, json=request_data)
-
-    assert response.status_code == 400, f"Unexpected status code: {response.status_code}, Response: {response.text}"
-
-    validate(response.json(), UNSUCCESSFUL_REGISTER_SCHEME)
-
-    response_data = response.json()
-    assert response_data["error"] == "Missing password", f"Unexpected error message: {response_data['error']}"
+    with allure.step('Отправка POST-запроса для регистрации пользователя без пароля'):
+        response = httpx.post(BASE_URL + REGISTER_USERS, json=request_data)
+    with allure.step('Проверка, что статус ответа равен 400'):
+        assert response.status_code == 400, f"Unexpected status code: {response.status_code}, Response: {response.text}"
+    with allure.step('Проверка ответа на соответствие JSON-схеме ошибки регистрации'):
+        validate(response.json(), UNSUCCESSFUL_REGISTER_SCHEME)
+    with allure.step('Проверка сообщения об ошибке в теле ответа'):
+        response_data = response.json()
+        assert response_data["error"] == "Missing password", f"Unexpected error message: {response_data['error']}"
